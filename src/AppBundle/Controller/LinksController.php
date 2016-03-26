7b7213143b0d08cb7914fc1defc26676bae5a7fd
Kevin Vandenborne <kevin@yappa.be>
2016-03-26 18:43:29 +0100

    Added Nelmio api docs

--- src/AppBundle/Controller/LinksController.php
+++ src/AppBundle/Controller/LinksController.php
@@ -2,19 +2,28 @@
 
 namespace AppBundle\Controller;
 
-
 use AppBundle\Entity\Link;
-use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\Annotations\View;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LinksController extends Controller
 {
     /**
-     * @Route("/v1/links.{_format}")
+     * @Route("/v1/links.{_format}", methods={"GET"})
      * @View()
+     * @ApiDoc(
+     *     description="Get all links",
+     *     authentication=true,
+     *     requirements={
+     *          {
+     *               "name"="apiKey",
+     *               "dataType"="string",
+     *               "description"="Your API key"
+     *          }
+     *     }
+     * )
      */
     public function getLinksAction()
     {
@@ -23,11 +32,27 @@ class LinksController extends Controller
     }
 
     /**
-     * @Route("/v1/link/{code}.{_format}")
+     * @Route("/v1/link/{code}.{_format}", methods={"GET"})
      * @View()
      * @param string $code
      * @return array
      * @internal param Link $link
+     * @ApiDoc(
+     *     description="Get link by code",
+     *     authentication=true,
+     *     requirements={
+     *          {
+     *               "name"="apiKey",
+     *               "dataType"="string",
+     *               "description"="Your API key"
+     *          },
+     *          {
+     *               "name"="code",
+     *               "dataType"="string",
+     *               "description"="Short URL code"
+     *          }
+     *     }
+     * )
      */
     public function getLinkAction(string $code) : array
     {
