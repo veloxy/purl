fbad2880d597df564d2271ac5193d7a7ff90d241
Kevin Vandenborne <kevin@yappa.be>
2016-03-28 14:11:13 +0200

    Generate short code on save

---
+++ src/AppBundle/Service/CodeGeneratorService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Service;
+
+use Hashids\Hashids;
+
+class CodeGeneratorService
+{
+    private $salt;
+
+    /**
+     * CodeGeneratorService constructor.
+     * @param $salt
+     */
+    public function __construct($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    public function generateCode()
+    {
+        $hashIds = new Hashids($this->salt);
+        return $hashIds->encode(time());
+    }
+}
