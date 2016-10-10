e5eb305f6ec7232919e3eb42749ed4cf154eb9e9
Kevin Vandenborne <kevin.vandenborne@gmail.com>
2016-10-10 20:25:55 +0200

    Renamed combined.js to scripts.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var src = 'app/Resources/assets';
 gulp.task('scripts', function () {
   return gulp.src(src + '/js/*.js')
     .pipe(sourcemaps.init())
-    .pipe(concat('combined.js'))
+    .pipe(concat('scripts.js'))
     .pipe(gutil.env.type === 'prod' ? uglify() : gutil.noop())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('web/js'));
