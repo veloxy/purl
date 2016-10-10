ff99f9e5a35bcec09e7fcd3613d9b934b4942d8f
Kevin Vandenborne <kevin.vandenborne@gmail.com>
2016-10-10 19:27:53 +0200

    Fix indenting in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,11 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('default', ['scripts']);
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('app/Resources/assets/js/*.js')
-        .pipe(sourcemaps.init())
-        	.pipe(concat('combined.js'))
-        	//only uglify if gulp is ran with '--type prod'
-        	.pipe(gutil.env.type === 'prod' ? uglify() : gutil.noop())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('web/js'));
+gulp.task('scripts', function () {
+  return gulp.src('app/Resources/assets/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('combined.js'))
+    .pipe(gutil.env.type === 'prod' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('web/js'));
 });
